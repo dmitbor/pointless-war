a018cf6269e7d392ccbac1b4783599ef14a3b439
Dmitry Borisov <dmitryborisova@gmail.com>
2019-07-19 23:15:44 -0400

    Prepping missions for combat.

--- misc/mission.py
+++ misc/mission.py
@@ -5,12 +5,19 @@ class Mission():
     # 1 - Objective
     # 2 - Assasination
     type = 0
+    # Engagement Level of the Mission:
+    # 0 - Ignore All Enemies
+    # 1 - Only fight over Objective
+    # 2 - Return Fire when Attacked
+    # 3 - Actively Engage Enemies in range
+    eng_level = 1
     completed = False
 
     def __init__(self):
         self.target = None
         self.objectives = []
         self.type = 0
+        self.eng_level = 0
         self.completed = False
 
     def set_obj_capture(self, trgt_point):
@@ -20,6 +27,7 @@ class Mission():
         self.objectives.append("NCO_PROX")
         self.objectives.append("OBJ_PROX")
         self.objectives.append("OBJ_CAPT")
+        self.eng_level = 2
         self.completed = False
 
     def get_miss_type(self):
