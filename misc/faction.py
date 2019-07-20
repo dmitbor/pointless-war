95d56569e6b7538127ded26547ab35146fdc8d64
Dmitry Borisov <dmitryborisova@gmail.com>
2019-07-19 23:15:30 -0400

    Faction color getter

--- misc/faction.py
+++ misc/faction.py
@@ -8,3 +8,14 @@ class Faction():
 
     def get_fac_color(self):
         return self.faction_color
+
+    def get_fac_color_caps(self):
+        cap_color = []
+        for color in self.faction_color:
+            cur_color = color + 50
+            if cur_color > 255:
+                cur_color = 255 - 50
+            elif cur_color < 0:
+                cur_color = 50
+            cap_color.append(cur_color)
+        return cap_color
